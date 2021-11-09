41a19b71ef6465c172b17bffd14bae8454ae1411
Seetha Rami Reddy N <60418966+seetharamireddyn@users.noreply.github.com>
2021-11-09 20:43:14 +0530

    Added project stracture

---
+++ ns-cart-thrid/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return(
+        <>
+            <hr/>
+            <center>
+                <h5>&copy; Nutri Store - Nov 2021</h5>
+            </center>
+        </>
+    )
+}
+
+export default Footer;
---
+++ ns-cart-thrid/src/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+    return (
+        <>
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a href="#" class="navbar-brand">
+                            <svg width="120">
+                                <defs>
+                                    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                                    <stop offset="0%" style={{stopColor: "rgba(0,255,0)", stopOpacity: "1"}} />
+                                    <stop offset="100%" style={{stopColor: "rgba(255,0,0)", stopOpacity: "1"}} />
+                                    </linearGradient>
+                                </defs>
+                                <ellipse cx="50" cy="15" rx="50" ry="20" fill="url(#grad1)" />
+                                <text fill="#ffffff" font-size="15" font-family="Verdana" x="8" y="20">Nutri Store</text>
+                                Sorry, your browser does not support inline SVG.
+                            </svg>
+                        </a>
+
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="https://github.com/">Page 1</a></li>
+                            <li><a href="https://google.com/">Page 2</a></li>
+                            <li><a href="https://facebook.com/">Page 3</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <hr />
+        </>
+    )
+}
+
+export default Header;
